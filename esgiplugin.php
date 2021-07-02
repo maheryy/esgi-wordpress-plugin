aa30173a9e99f71daf5a9696a54f389c904644f9
Maxime Carluer <mcarluer@myges.fr>
2021-07-02 14:38:29 +0200

    Plugin init

---
+++ esgiplugin.php
@@ -0,0 +1,7 @@
+<?php
+/* 
+* Plugin Name: Panel Community
+* Author: Maxime Carluer, Prado Rasoanirahina, Julien Arbellini
+* Version: 0.1
+* Description: Plugin créé dans le cadre du projet de Panorama des CMS.
+*/
